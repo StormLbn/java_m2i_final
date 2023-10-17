79e285143108dc360dcba242e7b2e0011b4d6faa
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-10-17 12:18:59 +0200

    added method to add shows in DB ; working

--- filrouge_back/src/main/java/com/example/filrouge_back/services/PopulateDatabaseService.java
+++ filrouge_back/src/main/java/com/example/filrouge_back/services/PopulateDatabaseService.java
@@ -3,9 +3,9 @@ package com.example.filrouge_back.services;
 import com.example.filrouge_back.entities.Media;
 import com.example.filrouge_back.models.*;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import com.example.filrouge_back.entities.Genre;
-import com.example.filrouge_back.entities.Media;
 import com.example.filrouge_back.entities.MediaProfessional;
 import com.example.filrouge_back.entities.Professional;
 import com.example.filrouge_back.models.MediaType;
@@ -14,17 +14,18 @@ import com.example.filrouge_back.repositories.GenreRepository;
 import com.example.filrouge_back.repositories.MediaProfessionalRepository;
 import com.example.filrouge_back.repositories.MediaRepository;
 import com.example.filrouge_back.repositories.ProfessionalRepository;
-import com.fasterxml.jackson.databind.JsonNode;
-import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,28 +58,35 @@ public class PopulateDatabaseService {
 
     @PostConstruct
     public void populateDatabase() {
-//        if (mediaRepository.count() == 0) {
-        if (true) {
+        if (mediaRepository.count() == 0) {
             log.info("Database is empty");
 
-            List<String> idList = getIdList("movie");
-
+            List<String> idList = getTmdbIdList("movie");
             log.info("Filling with " + idList.size() + " movies data...");
-            int notFoundCount = 0;
+            int errorsCount = 0;
 
             for (String id : idList) {
                 try {
-                    log.info(id);
+                    // FIXME : voir pourquoi certaines données ne s'enregistrent pas
                     getMovie(id);
                 } catch (Exception e) {
-                    log.warn("An error occurred while adding the data");
-                    notFoundCount++;
+                    log.warn("An error occurred while adding the movie data");
+                    log.warn(e.getMessage());
+                    errorsCount++;
                 }
             }
-            log.info((100 - notFoundCount) + " movies added to DB");
+            log.info((100 - errorsCount) + " movies added to DB");
 
-            // TODO récupérer les séries
-//            getShows();
+            log.info("Filling with 100 shows data...");
+            errorsCount = 0;
+            try {
+                getShows();
+            } catch (Exception e) {
+                log.warn("An error occurred while adding the show data");
+                log.warn(e.getMessage());
+                errorsCount++;
+            }
+            log.info((100 - errorsCount) + " shows added to DB");
         }
     }
 
@@ -106,7 +114,36 @@ public class PopulateDatabaseService {
             log.warn("Movie with id " + id + " not found on betaseries");
             throw new Exception(e);
         }
-        // TODO sauvegarder OU retourner les films & leurs acteurs
+    }
+
+    public void getShows() throws Exception {
+
+        RestTemplate restTemplate = betaseriesBuilder.build();
+
+        try {
+            ShowApiResponse showsResponse = restTemplate.getForEntity("shows/list?limit=100&order=popularity", ShowApiResponse.class).getBody();
+
+            if (showsResponse != null) {
+
+                List<ShowApiResponse.Show> showsList = showsResponse.getShows();
+                log.info("List size : " + showsList.size());
+                int count = 0;
+                for (ShowApiResponse.Show showResponse : showsList) {
+                    Media show = saveShow(showResponse);
+                    log.info("Show n°" + ++count);
+                    ActorsApiResponse actorsResponse = restTemplate
+                            .getForEntity("shows/characters?id=" + showResponse.getId(), ActorsApiResponse.class).getBody();
+
+                    if (actorsResponse != null) {
+                        saveActors(actorsResponse, show);
+                    }
+                }
+            }
+
+        } catch (HttpClientErrorException e) {
+            log.warn("Show not found");
+            throw new Exception(e);
+        }
     }
 
     @Transactional
@@ -119,7 +156,7 @@ public class PopulateDatabaseService {
         movie.setPlot(response.getMovie().getSynopsis());
         movie.setImageUrl(response.getMovie().getPoster());
         movie.setReleaseDate(response.getMovie().getRelease_date());
-        movie.setDuration(response.getMovie().getLength()/60);
+        movie.setDuration(Math.round(response.getMovie().getLength()/60.0f));
 
         movie.setGenres(new ArrayList<>());
         for (String data : response.getMovie().getGenres()) {
@@ -166,7 +203,6 @@ public class PopulateDatabaseService {
             movie.getProfessionals().add(producer);
         }
 
-
         // TODO refactoriser dans une méthode !
         for (Person data : response.getMovie().getCrew().getWriters()) {
             Professional person = findOrSaveProfessional(data);
@@ -187,25 +223,79 @@ public class PopulateDatabaseService {
         return movie;
     }
 
+
     @Transactional
-    public void saveActors(ActorsApiResponse response, Media media) {
-        List<Person> personsList = response.getCharacters();
-        int actorsCount = Math.min(personsList.size(), 7);
-        for (int i = 0 ; i <= actorsCount ; i++) {
-            Person data = personsList.get(i);
+    public Media saveShow(ShowApiResponse.Show response) {
+
+        Media show = new Media();
+        show.setType(MediaType.SHOW);
+
+        show.setBetaseriesId(response.getId());
+        show.setTitle(response.getTitle());
+        show.setPlot(response.getDescription());
+        show.setSeasons(response.getSeasons());
+        show.setImageUrl(response.getImages().getPoster());
+        show.setReleaseDate(LocalDate.of(response.getCreation(), 1, 1));
+        show.setDuration(response.getLength());
+        show.setInProdution(response.getStatus().equals("Continuing"));
+
+        show.setGenres(new ArrayList<>());
+        for (String data : response.getGenres().values()) {
+            Genre genre;
+            if (genreRepository.existsByGenreName(data)) {
+                genre = genreRepository.findByGenreName(data);
+            } else {
+                genre = genreRepository.save(
+                        Genre.builder().genreName(data).build()
+                );
+            }
+
+            show.getGenres().add(genre);
+        }
+
+        mediaRepository.save(show);
+
+        show.setProfessionals(new ArrayList<>());
+        for (Person data : response.getShowrunners()) {
             Professional person = findOrSaveProfessional(data);
 
-            MediaProfessional actor = mediaProfessionalRepository.save(
+            MediaProfessional producer = mediaProfessionalRepository.save(
                     MediaProfessional.builder()
-                            .job(JobForMedia.ACTOR)
+                            .job(JobForMedia.PRODUCER)
                             .professional(person)
-                            .media(media)
+                            .media(show)
                             .build()
             );
 
-            media.getProfessionals().add(actor);
+            show.getProfessionals().add(producer);
         }
-        mediaRepository.save(media);
+
+        mediaRepository.save(show);
+
+        return show;
+    }
+
+    @Transactional
+    public void saveActors(ActorsApiResponse response, Media media) {
+        List<Person> personsList = response.getCharacters();
+        if (!personsList.isEmpty()) {
+            int actorsCount = Math.min(personsList.size(), 7);
+            for (int i = 0; i <= actorsCount; i++) {
+                Person data = personsList.get(i);
+                Professional person = findOrSaveProfessional(data);
+
+                MediaProfessional actor = mediaProfessionalRepository.save(
+                        MediaProfessional.builder()
+                                .job(JobForMedia.ACTOR)
+                                .professional(person)
+                                .media(media)
+                                .build()
+                );
+
+                media.getProfessionals().add(actor);
+            }
+            mediaRepository.save(media);
+        }
     }
 
     public Professional findOrSaveProfessional(Person data) {
@@ -228,37 +318,8 @@ public class PopulateDatabaseService {
         return person;
     }
 
-    public void getShows() {
-        log.info("Filling with shows data");
 
-        RestTemplate restTemplate = betaseriesBuilder.build();
-
-        List<String> idList = new ArrayList<>();
-
-        ResponseEntity<JsonNode> entityJson =
-                restTemplate.getForEntity("shows/list?limit=100&order=popularity", JsonNode.class);
-
-        entityJson.getBody().findPath("results").elements().forEachRemaining(e -> {
-            idList.add(e.findPath("id").asText());
-            // TODO récupérer les données intéressantes des séries
-        });
-
-
-
-        // TODO Récupérer les données betaseries
-        for (String id : idList) {
-
-            entityJson = restTemplate.getForEntity("movies/characters?id=" + id, JsonNode.class);
-            entityJson.getBody().findPath("results").elements().forEachRemaining(e -> {
-                // TODO récupérer les données intéressantes des acteurs
-            });
-
-            // TODO sauvegarder OU retourner les films & leurs acteurs
-        }
-    }
-
-
-    private List<String> getIdList(String type) {
+    private List<String> getTmdbIdList(String type) {
         RestTemplate restTemplate = tmdbBuilder.build();
 
         List<String> idList = new ArrayList<>();

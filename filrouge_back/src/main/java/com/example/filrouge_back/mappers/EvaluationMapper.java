c638ae5ef1f35a55ae273b83c46a6bb4e2af2dda
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-10-18 16:44:09 +0200

    Started refactoring service and controller methods

--- filrouge_back/src/main/java/com/example/filrouge_back/mappers/EvaluationMapper.java
+++ filrouge_back/src/main/java/com/example/filrouge_back/mappers/EvaluationMapper.java
@@ -1,20 +1,14 @@
-//package com.example.filrouge_back.mappers;
-//
-//
-//import com.example.filrouge_back.entities.Evaluation;
-//import com.example.filrouge_back.entities.UserEntity;
-//import com.example.filrouge_back.models.EvaluationDTO;
-//import com.example.filrouge_back.models.UserDTO;
-//import org.mapstruct.Mapper;
-//
-//@Mapper
-//public interface EvaluationMapper {
-//
-//
-//    Evaluation evaluationDTOToEvaluation(EvaluationDTO evaluationDTO);
-//
-//   EvaluationDTO evaluationToEvaluationDTO(Evaluation evaluation) {
-//
-//    }
-//}
-//
+package com.example.filrouge_back.mappers;
+
+
+import com.example.filrouge_back.entities.Evaluation;
+import com.example.filrouge_back.models.EvaluationDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface EvaluationMapper {
+    Evaluation evaluationDTOToEvaluation(EvaluationDTO evaluationDTO);
+
+    EvaluationDTO evaluationToEvaluationDTO(Evaluation evaluation);
+}
+
--- filrouge_back/src/main/java/com/example/filrouge_back/services/EvaluationService.java
+++ filrouge_back/src/main/java/com/example/filrouge_back/services/EvaluationService.java
@@ -1,53 +1,59 @@
-//package com.example.filrouge_back.services;
-//
-//import com.example.filrouge_back.entities.Evaluation;
-//import com.example.filrouge_back.entities.Media;
-//import com.example.filrouge_back.entities.UserEntity;
-//import com.example.filrouge_back.mappers.EvaluationMapper;
-//import com.example.filrouge_back.models.EvaluationDTO;
-//import com.example.filrouge_back.repositories.EvaluationRepository;
-//import com.example.filrouge_back.repositories.MediaRepository;
-//import com.example.filrouge_back.repositories.UserEntityRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//import java.util.UUID;
-//
-//@Service
-//public class EvaluationService {
-//    public EvaluationDTO createEvaluation(UUID userId, UUID mediaId, EvaluationDTO evaluationDTO) {
-//        Optional<UserEntity> userEntity = UserEntityRepository.findById(userId);
-//        Optional<Media> media = MediaRepository.findById(mediaId);
-//
-//        if (userEntity.isPresent() && media.isPresent()) {
-//            Evaluation evaluation = EvaluationMapper.evaluationDTOToEvaluation(evaluationDTO);
-//            evaluation.setUser(userEntity.get());
-//            evaluation.setMedia(media.get());
-//
-//            return EvaluationMapper.evaluationToEvaluationDTO(EvaluationMapper.save(evaluation));
-//        } else {
-//            // Gérer le cas où l'utilisateur ou le média n'a pas été trouvé, par exemple, en lançant une exception.
-//            return null;
-//        }
-//    }
-//
-//    public EvaluationDTO updateEvaluation(UUID evaluationId, UUID id, EvaluationDTO updatedEvaluationDTO) {
-//        Optional<Evaluation> existingEvaluation = EvaluationRepository.findById(evaluationId);
-//
-//        if (existingEvaluation.isPresent()) {
-//            Evaluation updatedEvaluation = Evaluation.evaluationDTOToEvaluation(updatedEvaluationDTO);
-//            // Vous n'avez pas besoin de copier manuellement les propriétés ici
-//            // Le mapper MapStruct s'en chargera
-//            updatedEvaluation.setId(existingEvaluation.get().getId());
-//
-//            return evaluationMapper.evaluationToEvaluationDTO(EvaluationRepository.save(updatedEvaluation));
-//        } else {
-//
-//            return null;
-//        }
-//    }
-//
-//    public boolean deleteEvaluation(UUID userId, UUID evaluationId) {
-//    }
-//}
+package com.example.filrouge_back.services;
+
+import com.example.filrouge_back.entities.Evaluation;
+import com.example.filrouge_back.entities.Media;
+import com.example.filrouge_back.entities.UserEntity;
+import com.example.filrouge_back.mappers.EvaluationMapper;
+import com.example.filrouge_back.models.EvaluationDTO;
+import com.example.filrouge_back.repositories.EvaluationRepository;
+import com.example.filrouge_back.repositories.MediaRepository;
+import com.example.filrouge_back.repositories.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class EvaluationService {
+
+    private final UserEntityRepository userEntityRepository;
+    private final MediaRepository mediaRepository;
+    private final EvaluationRepository evaluationRepository;
+    private final EvaluationMapper evaluationMapper;
+
+    public EvaluationDTO createEvaluation(EvaluationDTO evaluationDTO) {
+        Optional<UserEntity> userEntity = userEntityRepository.findById(evaluationDTO.getUserId());
+        Optional<Media> media = mediaRepository.findById(evaluationDTO.getMediaId());
+
+        if (userEntity.isPresent() && media.isPresent()) {
+            Evaluation evaluation = evaluationMapper.evaluationDTOToEvaluation(evaluationDTO);
+            evaluation.setUser(userEntity.get());
+            evaluation.setMedia(media.get());
+
+            return evaluationMapper.evaluationToEvaluationDTO(evaluationRepository.save(evaluation));
+        } else {
+            // Gérer le cas où l'utilisateur ou le média n'a pas été trouvé.
+            return null;
+        }
+    }
+
+    public EvaluationDTO updateEvaluation(UUID evaluationId, EvaluationDTO updatedEvaluationDTO) {
+        Optional<Evaluation> existingEvaluation = evaluationRepository.findById(evaluationId);
+
+        if (existingEvaluation.isPresent()) {
+            Evaluation updatedEvaluation = evaluationMapper.evaluationDTOToEvaluation(updatedEvaluationDTO);
+            updatedEvaluation.setId(existingEvaluation.get().getId());
+
+            return evaluationMapper.evaluationToEvaluationDTO(evaluationRepository.save(updatedEvaluation));
+        } else {
+
+            return null;
+        }
+    }
+
+    public boolean deleteEvaluation(UUID evaluationId) {
+        return false;
+    }
+}

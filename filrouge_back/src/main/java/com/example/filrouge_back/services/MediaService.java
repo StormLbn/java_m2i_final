e72345cd75aa3184274fcf84f35b2e761fa5041c
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-10-20 09:45:02 +0200

    Moved method to calculate rating in mapping

--- filrouge_back/src/main/java/com/example/filrouge_back/services/MediaService.java
+++ filrouge_back/src/main/java/com/example/filrouge_back/services/MediaService.java
@@ -3,7 +3,6 @@ package com.example.filrouge_back.services;
 import com.example.filrouge_back.exceptions.ResourceNotFoundException;
 import com.example.filrouge_back.models.entitydtos.MediaDetailDTO;
 import com.example.filrouge_back.models.entitydtos.MediaSummaryDTO;
-import com.example.filrouge_back.entities.Evaluation;
 import com.example.filrouge_back.mappers.MediaMapper;
 import com.example.filrouge_back.entities.Media;
 import com.example.filrouge_back.models.enums.MediaType;
@@ -22,31 +21,9 @@ public class MediaService {
     private final MediaRepository mediaRepository;
     private final MediaMapper mediaMapper;
 
-
-    // TODO refactor : à faire dans le mapping ?
-
-    private void calculateAvgRating(Media media) {
-        List<Evaluation> evaluations = media.getEvaluations();
-        if (evaluations != null && !evaluations.isEmpty()) {
-            int sum = 0;
-            for (Evaluation evaluation : evaluations) {
-                if (evaluation.getRating() != null) {
-                    sum += evaluation.getRating();
-                }
-            }
-            double avgRating = (double) sum / evaluations.size();
-            media.setAvgRating(avgRating);
-        } else {
-            media.setAvgRating(0.0);
-        }
-    }
-
-
     public List<MediaSummaryDTO> getAllMedia() {
         List<Media> mediaList = mediaRepository.findAll();
-        for (Media media : mediaList) {
-            calculateAvgRating(media); // Calculer la moyenne des évaluations pour chaque média
-        }
+
         return mediaMapper.mediaListToMediaSummaryDtoList(mediaList);
     }
 
@@ -54,8 +31,6 @@ public class MediaService {
         Optional<Media> optionalMedia = mediaRepository.findById(mediaId);
 
         if (optionalMedia.isPresent()) {
-            Media media = optionalMedia.get();
-            calculateAvgRating(media); // Calculer la moyenne des évaluations
             return mediaMapper.mediaToMediaDetailDto(optionalMedia.get());
         } else {
             throw new ResourceNotFoundException("Media not found at id " + mediaId);
@@ -64,25 +39,18 @@ public class MediaService {
 
     public List<MediaSummaryDTO> getMediaByGenre(String genre) {
         List<Media> mediaList = mediaRepository.findByGenres_GenreName(genre);
-        for (Media media : mediaList) {
-            calculateAvgRating(media);
-        }
+
         return mediaMapper.mediaListToMediaSummaryDtoList(mediaList);
     }
 
     public List<MediaSummaryDTO> getMediaByType(MediaType type) {
         List<Media> mediaList = mediaRepository.findByType(type);
-        for (Media media : mediaList) {
-            calculateAvgRating(media);
-        }
+
         return mediaMapper.mediaListToMediaSummaryDtoList(mediaList);
     }
 
     public List<MediaSummaryDTO> getMediaByReleaseDateDescending() {
         List<Media> mediaList = mediaRepository.findAllByOrderByReleaseDateDesc();
-        for (Media media : mediaList) {
-            calculateAvgRating(media);
-        }
         return mediaMapper.mediaListToMediaSummaryDtoList(mediaList);
     }
 

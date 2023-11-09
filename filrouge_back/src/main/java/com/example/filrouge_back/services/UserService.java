7eb12a537d8be30d39385833d53ebf1f8ad33800
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-11-09 15:19:12 +0100

    refactored services : moved user favorites genres PATCH to user service

--- filrouge_back/src/main/java/com/example/filrouge_back/services/UserService.java
+++ filrouge_back/src/main/java/com/example/filrouge_back/services/UserService.java
@@ -3,14 +3,18 @@ package com.example.filrouge_back.services;
 
 import com.example.filrouge_back.entities.Genre;
 import com.example.filrouge_back.entities.UserEntity;
+import com.example.filrouge_back.exceptions.NullOrMissingAttributeException;
 import com.example.filrouge_back.exceptions.ResourceNotFoundException;
 import com.example.filrouge_back.mappers.UserMapper;
+import com.example.filrouge_back.models.entitydtos.GenreEditDTO;
 import com.example.filrouge_back.models.entitydtos.UserDisplayDTO;
 import com.example.filrouge_back.models.entitydtos.UserEditDTO;
+import com.example.filrouge_back.repositories.GenreRepository;
 import com.example.filrouge_back.repositories.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -21,43 +25,47 @@ public class UserService {
 
     private final UserEntityRepository userEntityRepository;
     private final UserMapper userMapper;
+    private final GenreService genreService;
 
     public UserDisplayDTO getUserById(UUID userId) {
-        Optional<UserEntity> foundUser = userEntityRepository.findById(userId);
-
-        if (foundUser.isPresent()) {
-            return userMapper.userToUserDisplayDto(foundUser.get());
-        } else {
-            throw new ResourceNotFoundException("User not found at id " + userId);
-        }
+        return userMapper.userToUserDisplayDto(findUserById(userId));
     }
 
     public UserEditDTO updateUser(UUID userId, UserEditDTO updatedUserDTO) {
-        Optional<UserEntity> optionalUser = userEntityRepository.findById(userId);
+        UserEntity user = findUserById(userId);
 
-        if (optionalUser.isPresent()) {
-            UserEntity user = optionalUser.get();
+        if (updatedUserDTO.getMail() != null) {
+            user.setMail(updatedUserDTO.getMail());
+        }
+        if (updatedUserDTO.getPseudo() != null) {
+            user.setPseudo(updatedUserDTO.getPseudo());
+        }
+        if (updatedUserDTO.getBirthDate() != null) {
+            user.setBirthDate(updatedUserDTO.getBirthDate());
+        }
+        if (updatedUserDTO.getPassword() != null) {
+            user.setPassword(updatedUserDTO.getPassword());
+        }
+        userEntityRepository.save(user);
 
-            if (updatedUserDTO.getMail() != null) {
-                user.setMail(updatedUserDTO.getMail());
-            }
-            if (updatedUserDTO.getPseudo() != null) {
-                user.setPseudo(updatedUserDTO.getPseudo());
-            }
-            if (updatedUserDTO.getBirthDate() != null) {
-                user.setBirthDate(updatedUserDTO.getBirthDate());
-            }
-            if (updatedUserDTO.getPassword() != null) {
-                user.setPassword(updatedUserDTO.getPassword());
-            }
-            userEntityRepository.save(user);
+        return userMapper.userToUserEditDto(user);
+    }
 
-            return userMapper.userToUserEditDto(user);
+    public UserDisplayDTO editUserGenresList(UUID userId, GenreEditDTO newGenres) {
+        UserEntity user = findUserById(userId);
+
+        if (newGenres.getGenreIdList() != null) {
+            user.setGenres(genreService.getGenresById(newGenres.getGenreIdList()));
+            return userMapper.userToUserDisplayDto(userEntityRepository.save(user));
         } else {
-            throw new ResourceNotFoundException("User not found at id " + userId);
+            throw new NullOrMissingAttributeException("New genres Id list is null");
         }
     }
 
+    private UserEntity findUserById(UUID userId) {
+        return userEntityRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found at id " + userId));
+    }
 }
 
 

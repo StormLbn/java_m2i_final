c1335ad8c66e0104a6e54a6f2624ac20310891ad
guillaume806 <guillaumebodart@outlook.fr>
2023-11-10 16:23:43 +0100

    firstcom

--- filrouge_front/src/app/app.module.ts
+++ filrouge_front/src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// app.module.ts
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaHomePageComponent } from './pages/media-home-page/media-home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaHomePageComponent
   ],
   imports: [
     BrowserModule,

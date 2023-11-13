a659868d83985f6663372595fb8911a6123c0fac
S. Lebrun <sophie.amelie_59@hotmail.com>
2023-11-13 10:05:42 +0100

    angular : added HTML forms for login/signup

--- filrouge_front/src/app/app-routing.module.ts
+++ filrouge_front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationPageComponent } from './pages/authentication-page/authentication-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth', component: AuthenticationPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- filrouge_front/src/app/app.module.ts
+++ filrouge_front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationPageComponent } from './pages/authentication-page/authentication-page.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationPageComponent,
+    LoginFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
